5347b2ea8f225274ab87571e53bb4bf576476de5
joshua <joshua.feryll@gmail.com>
2018-05-21 17:59:18 +0900

    transaction processing

---
+++ src/transaction.js
@@ -0,0 +1,86 @@
+const CryptoJS = require("crypto-js"),
+    elliptic = require("elliptic"),
+    utils = require("./utils");
+
+const ec = new elliptic.ec("secp256k1");
+
+class TxOut {
+    constructor(address, amount) {
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class TxIn {
+    // uTxOutId
+    // uTxOutIndex
+    // Signature
+}
+
+class Transaction {
+    // ID
+    // txIns[]
+    // txOuts[]
+}
+
+class UTxOut {
+    constructor(txOutId, txOutIndex, address, amount) {
+        this.TxOutId = txOutId;
+        this.TxOutIndex = txOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+let uTxOuts = [];
+
+const getTxId = tx => {
+    const txInContent = tx.txIns
+        .map(txIn => txIn.uTxOutId + txIn.txOutIndex)
+        .reduce((a, b) => a + b, "");
+    const txOutContent = tx.txOuts
+        .map(txOut => txOut.address + txOut.amount)
+        .reduce((a, b) => a + b, "");
+    return CryptoJS.SHA256(txUnContent + txOutContent).toString();
+};
+
+const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
+    return uTxOutList.find(uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex);
+}
+
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+    const txIn = tx.txIns[txInIndex];
+    const dataToSign = tx.id;
+    
+    const referencedUTxOut = findUTxOut(txIn.txOutId, txIn.txOutIndex, uTxOuts);
+    if(referencedUTxOut === null) {
+        return;
+    }
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const signature = utils.toHexString(key.sign(dataToSign).toDER());
+    return signature;
+};
+
+const updateUTxOuts = (newTxs, uTxOutList) => {
+    
+    // making new TxOut coming from a new Tx
+    const newUTxOuts = newTxs.map(tx => {
+        tx.txOuts.map(
+            (txOut, index) => {
+                new UTxOut(tx.id, index, txOut.address, txOut.amount);
+            });
+    })
+    .reduce((a, b) => a.concat(b), []);
+
+    // emptying the spentTxOut
+    const spentTxOuts = newTxs
+        .map(tx => tx.txIns)
+        .reduce((a, b) => a.concat(b), [])
+        .map(txIn => new UTxOut(txIn.txOutId, txIn.txOutIndex, "", 0));
+
+    // remove the spentTxOut and add the new TxOut
+    const resultingUTxOuts = uTxOutList
+        .filter(uTxO => !findUTxOut(uTxO.txOutId, uTxO.txOutIndex, spentTxOuts))
+        .concat(newUTxOuts);
+};
+

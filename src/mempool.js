3c167a28bcb2f3f65ee460e5e9bc4a4e55b76dc2
joshua <joshua.feryll@gmail.com>
2018-05-24 14:13:55 +0900

    validating transactions for mempool

--- src/mempool.js
+++ src/mempool.js
@@ -4,3 +4,26 @@ const _ = require("lodash"),
 const { validateTx } = Transactions;
 
 let memPool = [];
+
+const getTxInPool = memPool => {
+    return _(memPool).map(tx => tx.Ins).flatten().value();
+};
+
+const isTxValidForPool = (tx, memPool) => {
+    const txInsInPool = getTxInPool(memPool);
+    const isTxInAlreadyInPool = (txIns, txIn) => {
+        return _.find(txIns, txInsInPool => {
+            return (
+                txIn.txOutIndex === txInsInPool.txOutIndex &&
+                txIn.txOutId === txInsInPool.txOutId
+            );
+        });
+    }
+
+    for (const txIn of tx.txIns) {
+        if(isTxInAlreadyInPool(txInsInPool, txIn)) {
+            return false;
+        }
+    }
+    return true;
+};

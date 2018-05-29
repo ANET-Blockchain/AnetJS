38b70a062c3718facd177aa847e1242713b98a2c
joshua <joshua.feryll@gmail.com>
2018-05-29 11:00:33 +0900

    get address

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const express = require("express"),
 
 const { getBlockchain, createNewBlock, getAccountBalance, sendTx } = Blockchain;
 const { startP2PServer, connectToPeers } = P2P;
-const { initWallet } = Wallet;
+const { initWallet, getPublicFromWallet } = Wallet;
 const { getMempool } = Mempool;
 
 // Psssst. Don't forget about typing 'export HTTP_PORT=4000' in your console
@@ -39,10 +39,14 @@ app.get("/me/balance", (req, res) => {
   res.send({ balance });
 });
 
+
+app.get("/me/address", (req, res) => {
+  res.send(getPublicFromWallet());
+});
+
 app
   .route("/transactions")
   .get((req, res) => {
-    //console.log(getMempool());
     res.send(getMempool());
   })
   .post((req, res) => {
